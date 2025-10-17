e3fb51b9862800acfc0f3ba6f71d1520d65fab63
heonilha <heonil0304@gmail.com>
2025-10-17 18:58:28 +0900

    IDLE 애니메이션 구현

--- boy.py
+++ boy.py
@@ -1,5 +1,28 @@
 from pico2d import load_image, get_time
 from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
+from state_machine import StateMachine
+
+class Idle:
+
+    def __init__(self, boy):
+        self.boy = boy
+
+    def enter(self,e):
+        self.boy.dir = 0
+        self.boy.wait_start_time = get_time()
+    def exit(self,e):
+        pass
+
+    def do(self):
+        self.boy.frame = (self.boy.frame + 1) % 8
+        if get_time() - self.boy.wait_start_time > 2.0:
+            self.boy.state_machine.handle_state_event(('TIME_OUT', None))
+
+    def draw(self):
+        if self.boy.face_dir == 1: # right
+            self.boy.image.clip_draw(self.boy.frame * 100, 300, 100, 100, self.boy.x, self.boy.y)
+        else: # face_dir == -1: # left
+            self.boy.image.clip_draw(self.boy.frame * 100, 200, 100, 100, self.boy.x, self.boy.y)
 
 class Boy:
     def __init__(self):
@@ -8,13 +31,11 @@ class Boy:
         self.frame = 0
         self.dir = 0
         self.face_dir = 1
-
+        self.IDLE = Idle(self)
+        self.state_machine= StateMachine(self.IDLE)
     def update(self):
-        self.frame = (self.frame + 1) % 8
+        self.state_machine.update()
 
     def draw(self):
-        if self.face_dir == 1:
-            self.image.clip_draw(self.frame * 100, 100, 100, 100, self.x, self.y)
-        else:
-            self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        self.state_machine.draw()
     pass
