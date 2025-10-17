d504020b8ac8a74d3a77abca4b38065d832c9031
heonilha <heonil0304@gmail.com>
2025-10-17 17:39:29 +0900

    main함수 뼈대 작성

---
+++ main.py
@@ -0,0 +1,31 @@
+from pico2d import *
+
+open_canvas()
+
+
+def reset_world():
+    pass
+
+running=True
+
+reset_world()
+# game loop
+def handle_events():
+    pass
+
+
+def update_world():
+    pass
+
+
+def render_world():
+    pass
+
+
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+# finalization code
+close_canvas()
